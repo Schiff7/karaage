c47d42f9352f9013b7819657008c7da54ff09a49
Hu <hu@Hu.local>
2019-03-17 16:12:24 +0800

    Little work on home page

--- static/js/karaage/src/App.js
+++ static/js/karaage/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section><Nav /></section>
     );
   }
+}
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <h2 className="align-center" data-text="Hello, World!"><span>{"Hello, World!"}</span></h2>
+      <ul className="without-list-style align-center cursor-pointer">
+        <li><a className="underline" href="/posts">POSTS</a></li>
+        <li><a className="underline" href="/categories">CAREGORIES</a></li>
+        <li><a className="underline" href="/tags">TAGS</a></li>
+        <li><a className="underline" href="/about">ABOUT</a></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default App;
