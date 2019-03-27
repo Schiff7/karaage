ea96489a54139572d1196b783856b5cdf88d976a
Hu <apophisy@live.cn>
2019-03-27 22:00:56 +0800

    A simple animation improvment

--- static/js/karaage/src/App.js
+++ static/js/karaage/src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Transition } from 'react-transition-group';
 import './App.css';
 
 // Component
 const Nav = (props) => {
   return (
-    <nav className={`frame ${props.name}`}>
+    <nav>
       <h2 className='align-center' data-text='Hello, World!'><span>{'Hello, World!'}</span></h2>
       <ul className='without-list-style align-center cursor-pointer'>
         <li><Link className='underline' to='/posts'>POSTS</Link></li>
@@ -18,68 +19,69 @@ const Nav = (props) => {
 }
 
 const Paper = (props) => {
-  return <div className={`frame ${props.name}`}>PAPER</div>;
+  return <div>PAPER</div>;
 }
 
 const Posts = (props) => {
-  return <div className={`frame ${props.name}`}>POSTS</div>;
+  return <div>POSTS</div>;
 }
 
 const Categories = (props) => {
-  return <div className={`frame ${props.name}`}>CATEGORIES</div>;
+  return <div>CATEGORIES</div>;
 }
 
 const Tags = (props) => {
-  return <div className={`frame ${props.name}`}>TAGS</div>;
+  return <div>TAGS</div>;
 }
 
 const About = (props) => {
-  return <div className={`frame ${props.name}`}>ABOUT</div>;
-}
-
-const ViewTemplate = {
-  Default: function (props) {
-    return (<section className={`view ${props.name}`}>{props.children}</section>);
-  }
+  return <div>ABOUT</div>;
 }
 
 // Skeleton
 const SKELETON = {
-  views: [
-    { name: 'view-main' },
-    { name: 'view-right' },
-    { name: 'view-bottom' },
-  ],
+  views: {
+    main: { left: '0', top: '0' },
+    right: { left: '100%', top: '0' },
+    bottom: { left: '0', top: '100%' },
+  },
   frames: [
-    { name: 'frame-home', path: '/', exactPath: true, component: Nav, in: 'view-main' },
-    { name: 'frame-posts', path: '/posts/:query(category|tag|date)?/:keyword?', component: Posts, in: 'view-right' },
-    { name: 'frame-categories', path: '/categories', component: Categories, in: 'view-right' },
-    { name: 'frame-tags',path: '/tags', component: Tags, in: 'view-right' },
-    { name: 'frame-about', path: '/about', component: About, in: 'view-right' },
-    { name: 'frame-post', path: 'posts/:identifier', component: Paper, in: 'view-bottom' },
+    { name: 'frame-home', path: '/', exactPath: true, component: Nav, wrapper: 'main' },
+    { name: 'frame-posts', path: '/posts/:query(category|tag|date)?/:keyword?', component: Posts, wrapper: 'right' },
+    { name: 'frame-categories', path: '/categories', component: Categories, wrapper: 'right' },
+    { name: 'frame-tags',path: '/tags', component: Tags, wrapper: 'right' },
+    { name: 'frame-about', path: '/about', component: About, wrapper: 'right' },
+    { name: 'frame-post', path: 'posts/:identifier', component: Paper, wrapper: 'bottom' },
   ],
 };
 
-// Functions to render App
-function draw (skeleton) {
-  const { views, frames } = skeleton;
-  return views.map(view => {
-    const T = !!view.template ? ViewTemplate[view.template] : ViewTemplate.Default;
-    return (
-      <T name={view.name} key={view.name}>
-        {frames.filter(frame => frame.in === view.name)
-          .map(frame => { const C = frame.component; return <C name={frame.name} key={frame.name} />; })}
-      </T>
-    );
-  });
+const TransitionFrame = (props) => {
+  const { frame, transitionStyles } = props;
+  const C = frame.component;
+  const [inProp, setInProp] = useState(false);
+  useEffect(() => {
+    setInProp(true);
+  }, [inProp])
+  return (
+    <Transition in={inProp} key={frame.name} timeout={100}>
+      {state => <section className={`frame ${frame.name}`} style={{...transitionStyles.entering ,...transitionStyles[state]}}><C /></section>}
+    </Transition>
+  );
 }
 
-function hex (skeleton) {
-  return skeleton.frames.map(({ name, path, exactPath }) => <Route exact={!!exactPath} key={name} path={path} component={function () { 
-    const _skeleton = { ...skeleton, frames: skeleton.frames.map(frame => {
-      return frame.name === name ? { ...frame, in: 'view-main' } : frame;
-    })};
-    return draw(_skeleton);
+// Functions to render App
+function Hex ({ skeleton }) {
+  const transitionStyles = { entered: skeleton.views['main'] };
+  return skeleton.frames.map(({ name, path, exactPath }) => <Route key={`route-${name}`} exact={!!exactPath} path={path} component={function () { 
+    return <>{
+      skeleton.frames.map(frame => {
+      const C = frame.component;
+      if (frame.name === name) {
+        transitionStyles.entering = skeleton.views[frame.wrapper];
+        return <TransitionFrame key={frame.name} frame={frame} transitionStyles={transitionStyles} />;
+      }
+      return <section key={frame.name} className={`frame ${frame.name}`} style={skeleton.views[frame.wrapper]}><C /></section>;;
+    })}</>
   }} />);
 }
 
@@ -88,7 +90,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>{hex(SKELETON)}</Switch>
+        <Switch><Hex skeleton={SKELETON} /></Switch>
       </Router>
     );
   }
