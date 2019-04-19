d1a034334cb65f7c4391fa74f1605d6ae1d558ee
Hu <apophisy@live.cn>
2019-04-20 01:02:10 +0800

    Move shell builder to api folder and update now

---
+++ api/build.sh
@@ -0,0 +1,13 @@
+# build karaage
+
+files=`ls data`
+prefix="{\n  \"values\": ["
+suffix="\n  ]\n}"
+result=${prefix}
+for file in ${files}
+do
+  result="${result}\n    \"${file}\","
+done
+result="${result%,}${suffix}"
+mkdir build
+echo -e ${result} > build/posts.json
