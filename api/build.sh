64cc7f308b65e6efd772e6a69d45148edcfcfd42
nyctophilia <apophisy@live.cn>
2019-04-20 15:24:17 +0800

    Test: update shell scripts

--- api/build.sh
+++ api/build.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
 # build karaage
 
 files=`ls ../data`
-prefix="{\n  \"values\": ["
-suffix="\n  ]\n}"
+prefix="{\n\t\"values\": ["
+suffix="\n\t]\n}"
 result=${prefix}
 for file in ${files}
 do
-  result="${result}\n    \"${file}\","
+  file="${file%\.*}"
+  file="{ date: { y: ${file:0:4}, m: ${file:5:2}, d: ${file:8:2} }, slug: ${file:11} }"
+  result="${result}\n\t\t\"${file}\","
 done
 result="${result%,}${suffix}"
-mkdir build
+if [ ! -d "./build/" ]; then
+  mkdir build
+fi
 echo -e ${result} > build/posts.json
