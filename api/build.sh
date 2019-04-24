9395a6ab9fc002779b061c0ae477975dd14ecd71
nyctophilia <apophisy@live.cn>
2019-04-24 14:18:45 +0800

    Update build scripts

--- api/build.sh
+++ api/build.sh
@@ -9,12 +9,13 @@ suffix="\n\t]\n}"
 result=${prefix}
 for file in ${files}
 do
+  full_name=${file}
   full_file_path="${root_path}/data/${file}"
   tags_and_category=`sed -n '1p' ${full_file_path}`
   tags_and_category=${tags_and_category#*(}
   tags_and_category=${tags_and_category%)}
   file=${file%\.*}
-  file="{ \"date\": { \"y\": \"${file:0:4}\", \"m\": \"${file:5:2}\", \"d\": \"${file:8:2}\" }, \"slug\": \"${file:11}\", ${tags_and_category} }"
+  file="{ \"fullName\": \"${full_name}\", \"date\": { \"y\": \"${file:0:4}\", \"m\": \"${file:5:2}\", \"d\": \"${file:8:2}\" }, \"slug\": \"${file:11}\", ${tags_and_category} }"
   result="${result}\n\t\t${file},"
 done
 result="${result%,}${suffix}"
