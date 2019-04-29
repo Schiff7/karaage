9b9882713e1a54c343892e235d25f66c72970c61
Hu <apophisy@live.cn>
2019-04-30 00:54:56 +0800

    Update build scripts

--- api/build.sh
+++ api/build.sh
@@ -4,8 +4,8 @@
 current_path=$(cd $(dirname $0); pwd)
 root_path=${current_path%/*}
 files=`ls ${root_path}/data`
-prefix="{\n\t\"values\": ["
-suffix="\n\t]\n}"
+prefix="["
+suffix="\n]"
 result=${prefix}
 for file in ${files}
 do
@@ -16,11 +16,11 @@ do
   tags_and_category=${tags_and_category%)}
   file=${file%\.*}
   file="{ \"name\": \"${full_name}\", \"date\": { \"y\": \"${file:0:4}\", \"m\": \"${file:5:2}\", \"d\": \"${file:8:2}\" }, \"slug\": \"${file:11}\", ${tags_and_category} }"
-  result="${result}\n\t\t${file},"
+  result="${result}\n\t${file},"
 done
 result="${result%,}${suffix}"
 build_path="${root_path}/api/build"
 if [ ! -d ${build_path} ]; then
   mkdir ${build_path}
 fi
-echo -e ${result} > "${build_path}/posts.json"
+echo -e ${result} > "${build_path}/content.json"
