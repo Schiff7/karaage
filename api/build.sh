0b484185c4cf4a50afbce1d6e43edbffc2c10590
nyctophilia <apophisy@live.cn>
2019-05-06 11:10:40 +0800

    Update build scripts add comment

--- api/build.sh
+++ api/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# build karaage
+# Build karaage
+# List and read markdown files in the `/data/` directory,
+# to every file, a json object as follow:
+# { 
+#   name: string;
+#   date: { y: string; m: string; d: string; };
+#   slug: string;
+#   tags: Array<string>;
+#   category: string;
+# }
+# will be generated.
+# All generated objects will be collected to an array,
+# and the array will be written to the file `/api/build/content.json`.
+#
 
+# Path of the build file.
 current_path=$(cd $(dirname $0); pwd)
+# Path of project root.
 root_path=${current_path%/*}
 files=`ls ${root_path}/data`
 prefix="["
@@ -9,8 +24,12 @@ suffix="\n]"
 result=${prefix}
 for file in ${files}
 do
+  # File's full name.
   full_name=${file}
+  # Path to the current file.
   full_file_path="${root_path}/data/${file}"
+  # Read the first line of the file, which contains the current file's infomation of tags and category.
+  # e.g. [//]: # ("tags": [ "essay" ], "category": "Uncategorized")
   tags_and_category=`sed -n '1p' ${full_file_path}`
   tags_and_category=${tags_and_category#*(}
   tags_and_category=${tags_and_category%)}
@@ -19,8 +38,11 @@ do
   result="${result}\n\t${file},"
 done
 result="${result%,}${suffix}"
+# Where the output file should be.
 build_path="${root_path}/api/build"
+# Build the output directory if not exist.
 if [ ! -d ${build_path} ]; then
   mkdir ${build_path}
 fi
+# Redirect the STDOUT to the target file.
 echo -e ${result} > "${build_path}/content.json"
